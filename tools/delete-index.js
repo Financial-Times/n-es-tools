e9829bdd1286c4d3c9860fe44bf28826c1efe5ff
Jennifer Johnson <jkerr321@gmail.com>
2021-11-15 14:35:49 +0000

    linting

--- tools/delete-index.js
+++ tools/delete-index.js
@@ -2,11 +2,11 @@ const elastic = require('../lib/elastic')
 
 function run (cluster, command) {
   const { indexName } = command.opts()
-  
-  if(!indexName) {
+
+  if (!indexName) {
     console.log(`You have not provided a value for 'index'. Please provide a value for 'index' with the --index option before continuing`)
   }
-  
+
   const client = elastic(cluster)
   const clusterHost = global.workspace.clusters[cluster]
 
