9ba69d4ab5183c4ca608500e15b0d92e553871a7
Glynn Phillips <glynndominicphillips@gmail.com>
2021-11-16 10:05:03 +0000

    Style the reassign-alias output
    Giving this some colour/styling will make the output easier to read and help to distinguish errors from successful outputs

--- tools/reassign-alias.js
+++ tools/reassign-alias.js
@@ -1,3 +1,4 @@
+const chalk = require('chalk')
 const elastic = require('../lib/elastic')
 
 function createErrorMessage (value) {
@@ -20,7 +21,11 @@ function run (cluster, command) {
   const client = elastic(cluster)
   const clusterHost = global.workspace.clusters[cluster]
 
-  console.log(`Reassigning alias ${alias} from ${oldIndex} to ${newIndex} in ${cluster}: ${clusterHost}`)
+  console.log(chalk.cyan.bold.underline('Reassigning alias'))
+  console.log(`${chalk.cyan('Alias:')} ${alias}`)
+  console.log(`${chalk.cyan('From Index:')} ${oldIndex}`)
+  console.log(`${chalk.cyan('To Index:')} ${newIndex}`)
+  console.log(`${chalk.cyan('Cluster:')} ${cluster}: ${clusterHost}`)
 
   const actions = {
     'actions': [
@@ -31,11 +36,21 @@ function run (cluster, command) {
 
   return client.indices.updateAliases({ body: { actions } })
     .then(() => {
-      console.log(`Successfully reassigned alias ${alias} from ${oldIndex} to ${newIndex} in ${cluster}: ${clusterHost}`)
+      console.log(chalk.green.bold.underline('Alias reassigned'))
+      console.log(`${chalk.green('Alias:')} ${alias}`)
+      console.log(`${chalk.green('From Index:')} ${oldIndex}`)
+      console.log(`${chalk.green('To Index:')} ${newIndex}`)
+      console.log(`${chalk.green('Cluster:')} ${cluster}: ${clusterHost}`)
     })
     .catch(error => {
-      console.log(`Failed to reassign alias ${alias} from ${oldIndex} to ${newIndex} in ${cluster}: ${clusterHost}`)
-      console.log('error: ', error.meta.statusCode, error.meta.body.Message)
+      console.log(chalk.red.bold.underline('Failed to reassign alias'))
+
+      console.log(`${chalk.red('Alias:')} ${alias}`)
+      console.log(`${chalk.red('From Index:')} ${oldIndex}`)
+      console.log(`${chalk.red('To Index:')} ${newIndex}`)
+      console.log(`${chalk.red('Cluster:')} ${cluster}: ${clusterHost}`)
+
+      console.log(chalk.red('Error:'), error.meta.statusCode, error.meta.body.Message)
     })
 }
 
